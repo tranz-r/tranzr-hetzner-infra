edcca9a17b799c892acd7f67cffba37099ef6c79
Cloudsty <wi13b051@gmail.com>
2026-04-04 14:14:45 +0100

    fix: upgrade nginx gateway fabric specific crds

--- crds/main.tf
+++ crds/main.tf
@@ -16,6 +16,27 @@ resource "terraform_data" "nginx_gateway_api_crds" {
 }
 
 
+resource "terraform_data" "upgrade_nginx_gateway_api_crds" {
+  triggers_replace = [var.nginx_gateway_api_version]
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+
+    environment = {
+      KUBECONFIG = var.kubeconfig_path
+    }
+
+    command = <<-EOT
+      set -euo pipefail
+      test -f "$KUBECONFIG"
+      kubectl kustomize "https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/${var.nginx_gateway_api_version}/deploy/crds.yaml" | kubectl apply --server-side -f -
+    EOT
+  }
+
+  depends_on = [terraform_data.nginx_gateway_api_crds]
+}
+
+
+
 resource "helm_release" "external_secrets_operator" {
   name             = local.externalSecretsSettings.name
   namespace        = local.externalSecretsSettings.namespace
