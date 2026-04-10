609a1c2fedb549801f273923b761c7f0c194a763
Cloudsty <wi13b051@gmail.com>
2026-04-10 19:12:13 +0100

    feat: remove ingress nginx

--- infra-kube-hetzner/kube.tf
+++ infra-kube-hetzner/kube.tf
@@ -83,9 +83,6 @@ module "kube-hetzner" {
   cilium_hubble_enabled = true
   disable_kube_proxy    = true
 
-  ingress_controller = "none"
-  # ingress_target_namespace = "ingress-nginx"
-
   hetzner_ccm_use_helm = true
   hetzner_ccm_version  = "1.28.0"
 
